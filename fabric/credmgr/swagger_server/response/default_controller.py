492a05af54fe101b1d449b642786aa90210877e8
Komal Thareja <kthare10@renci.org>
2020-10-13 11:37:56 -0400

    update version

--- fabric/credmgr/swagger_server/response/default_controller.py
+++ fabric/credmgr/swagger_server/response/default_controller.py
@@ -1,4 +1,5 @@
 import connexion
+import requests
 import six
 
 from fabric.credmgr.swagger_server.models.version import Version  # noqa: E501
@@ -13,7 +14,20 @@ def version_get():  # noqa: E501
 
     :rtype: Version
     """
+    version = '1.0.0'
+    tag = '1.0.0'
+    url = "https://api.github.com/repos/fabric-testbesd/CredentialManager/git/refs/tags/{}".format(tag)
+
     response = Version()
-    response.version = '1.0.0'
-    response.gitsha1 = 'temp'
+    response.version = version
+    response.gitsha1 = 'Not Available'
+
+    result = requests.get(url)
+    if result.status_code == 200:
+        if result.json() is not None:
+            object_json = result.json().get("object", None)
+            if object_json is not None:
+                sha = object_json.get("sha", None)
+                if sha is not None:
+                    response.gitsha1 = sha
     return response
