991b1e014d3ff096fe874a63f448a010438da223
Yaxue Guo <yaxue.guo23@gmail.com>
2023-06-08 14:31:50 -0400

    #51: changed Header from class component to function component

--- cm-app/src/components/Header.jsx
+++ cm-app/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../imgs/fabric-brand.png";
 
-class Header extends React.Component {
-  handleLogin = () => {
+const Header = (props) => {
+  const handleLogin = () => {
     // remove old user status stored in browser.
     localStorage.removeItem("cmUserStatus");
     // nginx handle login url.
@@ -11,54 +11,52 @@ class Header extends React.Component {
   }
 
 
-  handleLogout = () => {
+  const handleLogout = () => {
     localStorage.removeItem("cmUserStatus");
     localStorage.removeItem("cmUserID");
     // nginx handle logout url.
     window.location.href = "/logout";
   }
 
-  render() {
-    return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light p-2 d-flex flex-row justify-content-between">
-        <NavLink className="navbar-brand" to="/">
-          {/* <img
-            src={logo}
-            width="70"
-            height="30"
-            className="d-inline-block align-top"
-            alt=""
-          /> */}
-          FABRIC Credential Manager
-        </NavLink>
-        <div className="ml-auto">
-          { 
-            this.props.cmUserStatus === "active" ?
-            <form className="form-inline my-2 my-lg-0">
-              <NavLink to="/logout">
-                <button
-                  onClick={() => this.handleLogout()}
-                  className="btn btn-outline-success my-2 my-sm-0"
-                >
-                  Log out
-                </button>
-              </NavLink>
-            </form> :
-            <form className="form-inline my-2 my-lg-0">
-              <NavLink to="/login">
-                <button
-                  onClick={() => this.handleLogin()}
-                  className="btn btn-outline-success my-2 my-sm-0 mr-2"
-                >
-                  Log in
-                </button>
-              </NavLink>
-            </form>
-          }
-        </div>
-      </nav>
-    );
-  }
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light p-2 d-flex flex-row justify-content-between">
+      <NavLink className="navbar-brand" to="/">
+        <img
+          src={logo}
+          width="70"
+          height="30"
+          className="d-inline-block align-top"
+          alt=""
+        />
+        FABRIC Credential Manager
+      </NavLink>
+      <div className="ml-auto">
+        { 
+          props.cmUserStatus === "active" ?
+          <form className="form-inline my-2 my-lg-0">
+            <NavLink to="/logout">
+              <button
+                onClick={handleLogout}
+                className="btn btn-outline-success my-2 my-sm-0"
+              >
+                Log out
+              </button>
+            </NavLink>
+          </form> :
+          <form className="form-inline my-2 my-lg-0">
+            <NavLink to="/login">
+              <button
+                onClick={handleLogin}
+                className="btn btn-outline-success my-2 my-sm-0 mr-2"
+              >
+                Log in
+              </button>
+            </NavLink>
+          </form>
+        }
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
